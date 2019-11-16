1ec1312190b44a519242e6fb935f491413cef06e
Philip Alava <z5205952@ad.unsw.edu.au>
2019-11-17 08:58:00 +1100

    Initial commit

---
+++ app/src/main/java/com/example/assignment/Trivia.java
@@ -0,0 +1,23 @@
+package com.example.assignment;
+
+public class Trivia {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    private int id;
+    private String question;
+
+}
---
+++ app/src/main/java/com/example/assignment/TriviaResponse.java
@@ -0,0 +1,7 @@
+package com.example.assignment;
+
+import java.util.ArrayList;
+
+public class TriviaResponse {
+    public ArrayList<Trivia> results;
+}
